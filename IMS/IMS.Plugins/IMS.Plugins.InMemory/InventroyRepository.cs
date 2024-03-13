dd9385aa3b89f28aa783b31a975293204cffab86
mdotpali <mostafa.palizdar@gmail.com>
2024-03-13 15:52:03 +0100

    initialize

---
+++ IMS/IMS.Plugins/IMS.Plugins.InMemory/InventroyRepository.cs
@@ -0,0 +1,32 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventroyRepository : IInventoryRepository
+    {
+        private List<Inventory> _inventories;
+
+        public InventroyRepository()
+        {
+            _inventories = new List<Inventory>()
+            {
+                new Inventory() {InventoryId = 1, InventoryName="Bike Seat", Quantity = 10, Price = 2},
+                new Inventory() {InventoryId = 2, InventoryName="Bike Body", Quantity = 10, Price = 15},
+                new Inventory() {InventoryId = 3, InventoryName="Bike Wheels", Quantity = 20, Price = 8},
+                new Inventory() {InventoryId = 4, InventoryName="Bike Pedels", Quantity = 20, Price = 1}
+            };
+        }
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name)) return await Task.FromResult(_inventories);
+
+            return _inventories.Where( x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
