c890e87257bd8165f4a99922a284e3dfdad8a3b3
Mostafa Palizdar <mostafa.palizdar@gmail.com>
2024-03-14 16:00:20 +0100

    added Add Inventory UseCase

--- IMS/IMS.CoreBusiness/Inventory.cs
+++ IMS/IMS.CoreBusiness/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,14 @@ namespace IMS.CoreBusiness
     public class Inventory
     {
         public int InventoryId {  get; set; }
+
+        [Required]
         public string InventoryName { get; set; } = string.Empty;
+        
+        [Range(0, int.MaxValue,ErrorMessage ="Quantity must be greater or equal to 0")]
         public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
         public double Price { get; set; }
 
     }
--- IMS/IMS.Plugins/IMS.Plugins.InMemory/InventroyRepository.cs
+++ IMS/IMS.Plugins/IMS.Plugins.InMemory/InventroyRepository.cs
@@ -22,6 +22,19 @@ namespace IMS.Plugins.InMemory
                 new Inventory() {InventoryId = 4, InventoryName="Bike Pedels", Quantity = 20, Price = 1}
             };
         }
+
+        public Task AddInventoryAsync(Inventory inventory)
+        {
+            if(_inventories.Any(x=> x.InventoryName.Equals(inventory.InventoryName, StringComparison.OrdinalIgnoreCase)))
+                return Task.CompletedTask;
+            
+            var maxId = _inventories.Max(x => x.InventoryId);
+            inventory.InventoryId = maxId + 1;
+
+            _inventories.Add(inventory);    
+            return Task.CompletedTask;  
+        }
+
         public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
         {
             if (string.IsNullOrEmpty(name)) return await Task.FromResult(_inventories);
